0edfa94f0b84527dc668b0b070ef9a9a2981bb0e
Dazante <zanta.david@gmail.com>
2026-06-28 12:57:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,45 @@
 const CACHE_NAME = 'synthlucida-v1';
+
+// Seznam souborů, které se uloží do paměti telefonu pro rychlé načítání
 const ASSETS_TO_CACHE = [
   './',
   './player.html',
   './manifest.json',
-  './icon.png'
-  // Zde můžeš přidat i další soubory, např. cesty k tvým .mp3 souborům, pokud je chceš mít offline
+  './icon.png',
+  './logo.jpg'
 ];
 
-// Instalace: Uloží soubory do mezipaměti
+// Instalace Service Workeru a uložení souborů do mezipaměti
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log('Ukládám do cache');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-// Aktivace: Odstraní staré verze cache
+// Aktivace a promazání staré cache (když v budoucnu změníš v1 na v2)
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) return caches.delete(cache);
+          if (cache !== CACHE_NAME) {
+            console.log('Mažu starou cache', cache);
+            return caches.delete(cache);
+          }
         })
       );
     })
   );
 });
 
-// Fetch: Pokud není internet, podívá se do cache
+// Získávání souborů (nejprve zkusí cache, pak internet)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
+      // Pokud soubor najde v paměti, vrátí ho hned. Jinak ho stáhne z netu.
       return response || fetch(event.request);
     })
   );
