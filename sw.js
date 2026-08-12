01b056d3936146aca591bef635c299d275803cd4
Dazante <zanta.david@gmail.com>
2026-08-12 22:16:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 // multi-redirect problem entirely.
 // ==========================================
 
-const APP_CACHE_NAME = 'synthlucida-app-v402';
+const APP_CACHE_NAME = 'synthlucida-app-v403';
 const AUDIO_CACHE_NAME = 'synthlucida-audio-v2'; // separate cache, survives app shell updates
 
 // App shell files cached on install
